551f40efbd3facf4e0b152b8d6af904c29ef7ea5
nakazi <nakazi1209@gmail.com>
2023-07-04 13:02:23 +0900

    説明文にアニメーション追加

--- Assets/Scripts/RedGaugeMove.cs
+++ Assets/Scripts/RedGaugeMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class RedGaugeMove : MonoBehaviour
+public class GaugeMove : MonoBehaviour
 {
     AudioSource audioSource;
     public AudioClip SE1;
