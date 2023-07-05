fc963ef724a39f4d0226b6090ee2a8b9e011512f
nakazi <nakazi1209@gmail.com>
2023-07-06 00:38:50 +0900

    遊び方説明完成

--- Assets/Scripts/SceneMove.cs
+++ Assets/Scripts/SceneMove.cs
@@ -10,12 +10,18 @@ public class SceneMove : MonoBehaviour
     public bool showExplain = false; 
     public GameObject textPanel;
     public GameObject explainPanel;
+    public GameObject explain_1;
+    public GameObject explain_2;
+    public GameObject explain_3;
+    public int explain_num = 1;
 
     // Start is called before the first frame update
     void Start()
     {
         explainPanel.SetActive(false);
         textPanel.SetActive(true);
+        explain_2.SetActive(false);
+        explain_3.SetActive(false);
         // Debug.Log(blueGaugeMove.gameOver);
         // Debug.Log(titleCanvasManager.showExplain);
 
@@ -28,8 +34,24 @@ public class SceneMove : MonoBehaviour
             explainPanel.SetActive(true);
             textPanel.SetActive(false);
             showExplain = true;
-        }else if(Input.GetKeyDown(KeyCode.Space)){
+        }else if(Input.GetKeyDown(KeyCode.RightArrow) && explain_num == 1){
+            explain_num = 2;
+            explain_1.SetActive(false);
+            explain_2.SetActive(true);
+        }else if(Input.GetKeyDown(KeyCode.RightArrow) && explain_num == 2){
+            explain_num = 3;
+            explain_2.SetActive(false);
+            explain_3.SetActive(true);
+        }else if(Input.GetKeyDown(KeyCode.RightArrow) && explain_num == 3){
             SceneManager.LoadScene("Game");
+        }else if(Input.GetKeyDown(KeyCode.LeftArrow) && explain_num == 3){
+            explain_num = 2;
+            explain_3.SetActive(false);
+            explain_2.SetActive(true);
+        }else if(Input.GetKeyDown(KeyCode.LeftArrow) && explain_num == 2){
+            explain_num = 1;
+            explain_2.SetActive(false);
+            explain_1.SetActive(true);
         }
     }
 }
